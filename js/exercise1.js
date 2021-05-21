c576f267e24ba8d76b6e39c62181bff8a07ad16c
yoann84800 <yoann.biliato@hotmail.fr>
2021-05-21 09:37:21 +0200

    exercisse1

---
+++ js/exercise1.js
@@ -0,0 +1,13 @@
+var livre1 = {
+    auteur : "yoann biliato",
+    titre: "javascript pour les boss",
+    reference: "4568"
+
+}
+​
+var livre1 = new Object();{
+livre1.fabricant = "javascript pour les boss";
+livre1.rigide = "carton";
+livre1.épaisseur = "papier de 88 pages";
+
+}
